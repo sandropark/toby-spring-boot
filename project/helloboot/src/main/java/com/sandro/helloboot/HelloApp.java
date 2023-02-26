483f4ae8843379924e45d269509ad20e8f4f9db3
Sandro Park <ticoba@naver.com>
2023-02-26 13:52:06 +0900

    스프링부트의 도움없이 서블릿 애플리케이션 띄우기 : 톰캣 띄우기

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -1,7 +1,12 @@
 package com.sandro.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 }
