0cbe86db3419063eb47ab9b9e79a8fd6e025afb3
Sandro Park <ticoba@naver.com>
2023-02-26 14:04:42 +0900

    스프링부트의 도움없이 서블릿 애플리케이션 띄우기 : 서블릿 등록

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -3,10 +3,25 @@ package com.sandro.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HelloApp {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType("text/plain");
+                    resp.getWriter().print("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 }
