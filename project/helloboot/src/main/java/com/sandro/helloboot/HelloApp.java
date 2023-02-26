83304d2b4e63373237b2f792b45149774107fa23
Sandro Park <ticoba@naver.com>
2023-02-26 13:46:03 +0900

    스프링부트의 도움없이 서블릿 애플리케이션 띄우기 : Init

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -1,11 +1,7 @@
 package com.sandro.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class HelloApp {
     public static void main(String[] args) {
-        SpringApplication.run(HelloApp.class, args);
+        System.out.println("Hello Containerless Standalone Application");
     }
 }
