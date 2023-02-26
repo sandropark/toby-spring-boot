7fe3b54a3055b51d609d8eaf5966cff7d47e81d6
Sandro Park <ticoba@naver.com>
2023-02-26 14:25:31 +0900

    스프링부트의 도움없이 서블릿 애플리케이션 띄우기 : 프론트 컨트롤러

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -2,6 +2,7 @@ package com.sandro.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -15,16 +16,22 @@ public class HelloApp {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");     // 파라미터를 받는다.
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");     // 파라미터를 받는다.
 
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().print("Hello " + name);
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().print("Hello " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");    // '/' 하위 모든 요청을 처리한다.
         });
         webServer.start();
     }
