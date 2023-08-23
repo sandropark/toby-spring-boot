c86ac5c24edba1036c8ed8bfa6fc7dea01cba35f
sandropark <ticoba@naver.com>
2023-08-23 09:41:37 +0900

    독립 실행형 스프링 애플리케이션 - Bean의 생명주기 메소드

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -1,6 +1,8 @@
 package com.sandro.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -13,16 +15,26 @@ public class HelloApp {
             protected void onRefresh() {
                 super.onRefresh();
 
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
                 serverFactory.getWebServer(servletContext ->
                         servletContext
-                                .addServlet("dispatcherServlet", new DispatcherServlet(this))
+                                .addServlet("dispatcherServlet", this.getBean(DispatcherServlet.class))
                                 .addMapping("/*")
                 ).start();
             }
         };
         applicationContext.register(HelloApp.class);
         applicationContext.refresh();   // register 후 refresh하면 빈이 생성된다.
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 
 }
