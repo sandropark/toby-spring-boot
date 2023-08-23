3362d6ba239b105278ce50a7acd15b78146531ef
sandropark <ticoba@naver.com>
2023-08-23 09:48:33 +0900

    독립 실행형 스프링 애플리케이션 - SpringBootApplication 2

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -1,18 +1,17 @@
 package com.sandro.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan
-@Configuration
 public class HelloApp {
 
     public static void main(String[] args) {
-        MySpringApplication.run(HelloApp.class, args);
+        SpringApplication.run(HelloApp.class, args);
     }
 
     @Bean
