bcc3fdbefd4a93c24871d5688dee52a5857e7c9a
sandropark <ticoba@naver.com>
2023-08-23 09:15:54 +0900

    독립 실행형 스프링 애플리케이션 - 스프링 컨테이너로 통합

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloApp.java
@@ -1,50 +1,27 @@
 package com.sandro.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloApp {
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = registerBeans();
-        new TomcatServletWebServerFactory()
-                .getWebServer(servletContext -> servletContext
-                        .addServlet("frontcontroller", getHttpServlet(applicationContext))
-                        .addMapping("/*")
-                ).start();
-    }
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
 
-    private static GenericApplicationContext registerBeans() {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                serverFactory.getWebServer(servletContext ->
+                        servletContext
+                                .addServlet("dispatcherServlet", new DispatcherServlet(this))
+                                .addMapping("/*")
+                ).start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();   // register 후 refresh하면 빈이 생성된다.
-        return applicationContext;
     }
 
-    private static HttpServlet getHttpServlet(GenericApplicationContext applicationContext) {
-        return new HttpServlet() {
-            @Override
-            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                // 인증, 보안, 다국어, 공통 기능
-                if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                    String name = req.getParameter("name");
-                    String ret = applicationContext.getBean(HelloController.class).hello(name);
-
-                    resp.getWriter().println(ret);
-                    resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                } else {
-                    resp.setStatus(HttpStatus.NOT_FOUND.value());
-                }
-            }
-        };
-    }
 }
