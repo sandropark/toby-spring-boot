e33e6b2dae32a3f748323e2e1adee8148fe98cf3
sandropark <ticoba@naver.com>
2023-08-23 09:15:43 +0900

    독립 실행형 스프링 애플리케이션 - 애너테이션 매핑 정보 사용

--- project/helloboot/src/main/java/com/sandro/helloboot/HelloController.java
+++ project/helloboot/src/main/java/com/sandro/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.sandro.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @ResponseBody
+    @GetMapping
     public String hello(String name) {
         // 컨트롤러는 요청 파라미터를 검증하는 책임을 갖고 있다. 따라서 name이 null이 아닌지 확인한다.
         return helloService.sayHello(Objects.requireNonNull(name));
