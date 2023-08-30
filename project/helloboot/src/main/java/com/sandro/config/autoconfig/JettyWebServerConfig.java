ee49638fda439905b9160550b87aa53da18c6c4c
sandropark <ticoba@naver.com>
2023-08-30 09:23:56 +0900

    자동 구성 기반 애플리케이션 - 자동 구성 정보 대체하기
    - @ConditionalOnMissingBean 을 사용해서 해당 타입의 빈이 등록되어 있는지 확인하고 없는 경우에만 빈을 등록하도록 설정

--- project/helloboot/src/main/java/com/sandro/config/autoconfig/JettyWebServerConfig.java
+++ project/helloboot/src/main/java/com/sandro/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package com.sandro.config.autoconfig;
 
 import com.sandro.config.ConditionalMyOnClass;
 import com.sandro.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 public class JettyWebServerConfig {
 
+    @ConditionalOnMissingBean
     @Bean(name = "jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
--- project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
+++ project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.sandro.config.autoconfig;
 
 import com.sandro.config.ConditionalMyOnClass;
 import com.sandro.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
 
+    @ConditionalOnMissingBean  // 해당 타입의 빈이 등록되어 있는 경우, 아래 빈 메서드는 실행되지 않는다.
     @Bean(name = "tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
