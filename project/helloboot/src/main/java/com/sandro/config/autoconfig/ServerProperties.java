547c81eb3e330811aee0513125359966ac79acec
sandropark <ticoba@naver.com>
2023-08-31 10:22:15 +0900

    자동 구성 기반 애플리케이션 - 프로퍼티 빈의 후처리기 도입

--- project/helloboot/src/main/java/com/sandro/config/autoconfig/ServerProperties.java
+++ project/helloboot/src/main/java/com/sandro/config/autoconfig/ServerProperties.java
@@ -1,8 +1,10 @@
 package com.sandro.config.autoconfig;
 
+import com.sandro.config.MyConfigurationProperties;
 import lombok.Data;
 
 @Data
+@MyConfigurationProperties(prefix = "server")
 public class ServerProperties {
     private String contextPath;
     private int port;
--- project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
+++ project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
@@ -1,12 +1,14 @@
 package com.sandro.config.autoconfig;
 
 import com.sandro.config.ConditionalMyOnClass;
+import com.sandro.config.EnableMyConfigurationProperties;
 import com.sandro.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+@EnableMyConfigurationProperties(ServerProperties.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
