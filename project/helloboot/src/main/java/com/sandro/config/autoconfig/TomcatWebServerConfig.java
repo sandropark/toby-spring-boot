e659f9b6a3d613f95c5ee5503d85b9201d0da1eb
sandropark <ticoba@naver.com>
2023-08-31 09:20:56 +0900

    자동 구성 기반 애플리케이션 - @Value와 PropertySourcesPlaceholderConfigurer
    - 직접 Environment를 주입받지 않고 @Value를 사용해서 환경변수 값을 주입받는 방식으로 변경
    - @Value가 환경변수를 치환하려면 PropertySourcesPlaceholderConfigurer 가 빈으로 등록되어 있어야 한다.

--- project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
+++ project/helloboot/src/main/java/com/sandro/config/autoconfig/TomcatWebServerConfig.java
@@ -2,21 +2,24 @@ package com.sandro.config.autoconfig;
 
 import com.sandro.config.ConditionalMyOnClass;
 import com.sandro.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @ConditionalOnMissingBean  // 해당 타입의 빈이 등록되어 있는 경우, 아래 빈 메서드는 실행되지 않는다.
     @Bean(name = "tomcatWebServerFactory")
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(env.getProperty("contextPath"));
+        serverFactory.setContextPath(contextPath);
         return serverFactory;
     }
 }
